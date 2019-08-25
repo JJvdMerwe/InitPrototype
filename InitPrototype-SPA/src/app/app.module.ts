de1fe84070a8f60ec72ea342606bcc9abe7ea9ec
jordaan <jorvdmerwe@gmail.com>
2019-08-25 20:31:42 +0200

    Add alertify and auth service

--- InitPrototype-SPA/src/app/app.module.ts
+++ InitPrototype-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
    declarations: [
@@ -12,6 +13,7 @@ import { NavComponent } from './nav/nav.component';
    ],
    imports: [
       BrowserModule,
+      HttpClientModule,
       AppRoutingModule
    ],
    providers: [],
