5e229fdafff30e5bd996f5a2c887ad122f32a68f
jordaan <jorvdmerwe@gmail.com>
2019-08-25 18:41:44 +0200

    Model prototyping

--- InitPrototype.DataAccess/Models/UserRoles/AdministratorRole.cs
+++ InitPrototype.DataAccess/Models/UserRoles/AdministratorRole.cs
@@ -1,7 +1,10 @@
+using InitPrototype.DataAccess.Models.Settings;
+
 namespace InitPrototype.DataAccess.Models
 {
     public class AdministratorRole
     {
         public int AdministratorRoleId { get; set; }
+        public AdministratorSettings AdministratorSettings { get; set; }
     }
 }
